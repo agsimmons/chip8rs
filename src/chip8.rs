509ac6ae0a65e9a87ffea88539c941c83beb12df
Andrew Simmons <agsimmons0@gmail.com>
2021-04-01 18:38:05 -0400

    Update docstrings

--- src/chip8.rs
+++ src/chip8.rs
@@ -214,7 +214,8 @@ impl Chip8 {
     /// 2nnn - CALL addr
     /// Call subroutine at nnn.
     ///
-    /// The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
+    /// The interpreter increments the stack pointer, then puts the current PC
+    /// on the top of the stack. The PC is then set to nnn.
     fn call_addr(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -222,7 +223,8 @@ impl Chip8 {
     /// 3xkk - SE Vx, byte
     /// Skip next instruction if Vx = kk.
     ///
-    /// The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
+    /// The interpreter compares register Vx to kk, and if they are equal,
+    /// increments the program counter by 2.
     fn se_vx_byte(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -230,7 +232,8 @@ impl Chip8 {
     /// 4xkk - SNE Vx, byte
     /// Skip next instruction if Vx != kk.
     ///
-    /// The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
+    /// The interpreter compares register Vx to kk, and if they are not equal,
+    /// increments the program counter by 2.
     fn sne_vx_byte(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -238,7 +241,8 @@ impl Chip8 {
     /// 5xy0 - SE Vx, Vy
     /// Skip next instruction if Vx = Vy.
     ///
-    /// The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
+    /// The interpreter compares register Vx to register Vy, and if they are
+    /// equal, increments the program counter by 2.
     fn se_vx_vy(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -257,7 +261,8 @@ impl Chip8 {
     /// 7xkk - ADD Vx, byte
     /// Set Vx = Vx + kk.
     ///
-    /// Adds the value kk to the value of register Vx, then stores the result in Vx.
+    /// Adds the value kk to the value of register Vx, then stores the result
+    /// in Vx.
     fn add_vx_byte(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -273,7 +278,10 @@ impl Chip8 {
     /// 8xy1 - OR Vx, Vy
     /// Set Vx = Vx OR Vy.
     ///
-    /// Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. A bitwise OR compares the corrseponding bits from two values, and if either bit is 1, then the same bit in the result is also 1. Otherwise, it is 0.
+    /// Performs a bitwise OR on the values of Vx and Vy, then stores the
+    /// result in Vx. A bitwise OR compares the corrseponding bits from two
+    /// values, and if either bit is 1, then the same bit in the result is
+    /// also 1. Otherwise, it is 0.
     fn or_vx_vy(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -281,7 +289,10 @@ impl Chip8 {
     /// 8xy2 - AND Vx, Vy
     /// Set Vx = Vx AND Vy.
     ///
-    /// Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. A bitwise AND compares the corrseponding bits from two values, and if both bits are 1, then the same bit in the result is also 1. Otherwise, it is 0.
+    /// Performs a bitwise AND on the values of Vx and Vy, then stores the
+    /// result in Vx. A bitwise AND compares the corrseponding bits from two
+    /// values, and if both bits are 1, then the same bit in the result is also
+    /// 1. Otherwise, it is 0.
     fn and_vx_vy(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -289,7 +300,10 @@ impl Chip8 {
     /// 8xy3 - XOR Vx, Vy
     /// Set Vx = Vx XOR Vy.
     ///
-    /// Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx. An exclusive OR compares the corrseponding bits from two values, and if the bits are not both the same, then the corresponding bit in the result is set to 1. Otherwise, it is 0.
+    /// Performs a bitwise exclusive OR on the values of Vx and Vy, then stores
+    /// the result in Vx. An exclusive OR compares the corrseponding bits from
+    /// two values, and if the bits are not both the same, then the
+    /// corresponding bit in the result is set to 1. Otherwise, it is 0.
     fn xor_vx_vy(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -297,14 +311,17 @@ impl Chip8 {
     /// 8xy4 - ADD Vx, Vy
     /// Set Vx = Vx + Vy, set VF = carry.
     ///
-    /// The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
+    /// The values of Vx and Vy are added together. If the result is greater
+    /// than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0. Only the lowest
+    /// 8 bits of the result are kept, and stored in Vx.
     fn add_vx_vy(&mut self, command: u16) {
         panic!("Not Implemented");
     }
     /// 8xy5 - SUB Vx, Vy
     /// Set Vx = Vx - Vy, set VF = NOT borrow.
     ///
-    /// If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
+    /// If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted
+    /// from Vx, and the results stored in Vx.
     fn sub_vx_vy(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -312,7 +329,8 @@ impl Chip8 {
     /// 8xy6 - SHR Vx {, Vy}
     /// Set Vx = Vx SHR 1.
     ///
-    /// If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+    /// If the least-significant bit of Vx is 1, then VF is set to 1, otherwise
+    /// 0. Then Vx is divided by 2.
     fn shr_xv(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -320,7 +338,8 @@ impl Chip8 {
     /// 8xy7 - SUBN Vx, Vy
     /// Set Vx = Vy - Vx, set VF = NOT borrow.
     ///
-    /// If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
+    /// If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted
+    /// from Vy, and the results stored in Vx.
     fn subn_vx_vy(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -328,7 +347,8 @@ impl Chip8 {
     /// 8xyE - SHL Vx {, Vy}
     /// Set Vx = Vx SHL 1.
     ///
-    /// If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
+    /// If the most-significant bit of Vx is 1, then VF is set to 1, otherwise
+    /// to 0. Then Vx is multiplied by 2.
     fn shl_vx(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -336,7 +356,8 @@ impl Chip8 {
     /// 9xy0 - SNE Vx, Vy
     /// Skip next instruction if Vx != Vy.
     ///
-    /// The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
+    /// The values of Vx and Vy are compared, and if they are not equal, the
+    /// program counter is increased by 2.
     fn sne_vx_vy(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -362,15 +383,26 @@ impl Chip8 {
     /// Cxkk - RND Vx, byte
     /// Set Vx = random byte AND kk.
     ///
-    /// The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx. See instruction 8xy2 for more information on AND.
+    /// The interpreter generates a random number from 0 to 255, which is then
+    /// ANDed with the value kk. The results are stored in Vx. See instruction
+    /// 8xy2 for more information on AND.
     fn rnd_vx_byte(&mut self, command: u16) {
         panic!("Not Implemented");
     }
 
     /// Dxyn - DRW Vx, Vy, nibble
-    /// Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+    /// Display n-byte sprite starting at memory location I at (Vx, Vy),
+    /// set VF = collision.
     ///
-    /// The interpreter reads n bytes from memory, starting at the address stored in I. These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). Sprites are XORed onto the existing screen. If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it is outside the coordinates of the display, it wraps around to the opposite side of the screen. See instruction 8xy3 for more information on XOR, and section 2.4, Display, for more information on the Chip-8 screen and sprites.
+    /// The interpreter reads n bytes from memory, starting at the address
+    /// stored in I. These bytes are then displayed as sprites on screen at
+    /// coordinates (Vx, Vy). Sprites are XORed onto the existing screen. If
+    /// this causes any pixels to be erased, VF is set to 1, otherwise it is
+    /// set to 0. If the sprite is positioned so part of it is outside the
+    /// coordinates of the display, it wraps around to the opposite side of the
+    /// screen. See instruction 8xy3 for more information on XOR, and
+    /// section 2.4, Display, for more information on the Chip-8 screen and
+    /// sprites.
     fn drw_vx_vy_nibble(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -378,7 +410,8 @@ impl Chip8 {
     /// Ex9E - SKP Vx
     /// Skip next instruction if key with the value of Vx is pressed.
     ///
-    /// Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
+    /// Checks the keyboard, and if the key corresponding to the value of Vx is
+    /// currently in the down position, PC is increased by 2.
     fn skp_vx(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -386,7 +419,8 @@ impl Chip8 {
     /// ExA1 - SKNP Vx
     /// Skip next instruction if key with the value of Vx is not pressed.
     ///
-    /// Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
+    /// Checks the keyboard, and if the key corresponding to the value of Vx is
+    /// currently in the up position, PC is increased by 2.
     fn sknp_vx(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -402,7 +436,8 @@ impl Chip8 {
     /// Fx0A - LD Vx, K
     /// Wait for a key press, store the value of the key in Vx.
     ///
-    /// All execution stops until a key is pressed, then the value of that key is stored in Vx.
+    /// All execution stops until a key is pressed, then the value of that key
+    /// is stored in Vx.
     fn ld_vx_k(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -434,7 +469,9 @@ impl Chip8 {
     /// Fx29 - LD F, Vx
     /// Set I = location of sprite for digit Vx.
     ///
-    /// The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx. See section 2.4, Display, for more information on the Chip-8 hexadecimal font.
+    /// The value of I is set to the location for the hexadecimal sprite
+    /// corresponding to the value of Vx. See section 2.4, Display, for
+    /// more information on the Chip-8 hexadecimal font.
     fn ld_f_vx(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -442,7 +479,9 @@ impl Chip8 {
     /// Fx33 - LD B, Vx
     /// Store BCD representation of Vx in memory locations I, I+1, and I+2.
     ///
-    /// The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.
+    /// The interpreter takes the decimal value of Vx, and places the hundreds
+    /// digit in memory at location in I, the tens digit at location I+1, and
+    /// the ones digit at location I+2.
     fn ld_b_vx(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -450,7 +489,8 @@ impl Chip8 {
     /// Fx55 - LD [I], Vx
     /// Store registers V0 through Vx in memory starting at location I.
     ///
-    /// The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
+    /// The interpreter copies the values of registers V0 through Vx into
+    /// memory, starting at the address in I.
     fn ld_i_vx(&mut self, command: u16) {
         panic!("Not Implemented");
     }
@@ -458,7 +498,8 @@ impl Chip8 {
     /// Fx65 - LD Vx, [I]
     /// Read registers V0 through Vx from memory starting at location I.
     ///
-    /// The interpreter reads values from memory starting at location I into registers V0 through Vx.
+    /// The interpreter reads values from memory starting at location I into
+    /// registers V0 through Vx.
     fn load_vx_i(&mut self, command: u16) {
         panic!("Not Implemented");
     }
