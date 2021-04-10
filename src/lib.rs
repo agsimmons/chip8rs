2e1b7e6f229ffe84ba229d7b926cd7c836c81b83
Andrew Simmons <agsimmons0@gmail.com>
2021-04-10 17:57:59 -0400

    Refactor config package layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,2 @@
-use std::env;
-
-pub struct Config {
-    pub rom_path: String,
-}
-
-impl Config {
-    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-        args.next();
-
-        let rom_path = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Rom path not specified"),
-        };
-
-        Ok(Config { rom_path })
-    }
-}
+mod config;
+pub use self::config::Config;
